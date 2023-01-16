e177e27c2f3b8978b168356bff018f316d83d590
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2023-01-16 08:33:11 +0000

    モデル

--- db/migrate/20230116075927_add_index_to_users.rb
+++ db/migrate/20230116075927_add_index_to_users.rb
@@ -1,7 +1,9 @@
 class AddIndexToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_index :items,[:id]
-    add_index :genre,[:id]
+    add_index :customers,:id
+    add_index :admins,:id
+    add_index :items,:id
+    add_index :genre,:id
 
   end
 end
