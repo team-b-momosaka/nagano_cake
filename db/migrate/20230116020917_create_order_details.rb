5e1129fccab761e75a1d7ea1366251ea5f13d2ae
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-01-16 08:09:10 +0000

    [Add]tabel-order/order_ditails

--- db/migrate/20230116020917_create_order_details.rb
+++ db/migrate/20230116020917_create_order_details.rb
@@ -1,6 +1,11 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      t.integer :order_id, null: false, foreign_key: true
+      t.integer :item_id, null: false, foreign_key: true
+      t.integer :purchase_price, null: false
+      t.integer :quantity, null: false
+      t.integer :production_status, null: false, :default => 0
 
       t.timestamps
     end
