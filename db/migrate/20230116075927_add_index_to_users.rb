fdfcb49314a53cce6912e8632ba86bed469ccfec
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2023-01-16 08:40:46 +0000

    アイテム、ジャンルカラム追加

--- db/migrate/20230116075927_add_index_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_index :customers,:id
-    add_index :admins,:id
-    add_index :items,:id
-    add_index :genre,:id
-
-  end
-end
